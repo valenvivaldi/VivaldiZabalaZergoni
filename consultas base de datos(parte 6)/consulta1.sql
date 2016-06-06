e389170b96f348b9112eb202ffea6339d1fee6a8
bzergoni <bruno_z_c@hotmail.com>
2016-06-06 19:36:32 -0300

    cambio

--- consultas base de datos(parte 6)/consulta1.sql
+++ consultas base de datos(parte 6)/consulta1.sql
@@ -1,4 +1,4 @@
-﻿select nombre_programa, count(frecuencia) 
+select nombre_programa, count(frecuencia) as Total_Aportes_Mensuales
 from ciudad_de_los_niños.aporta
 where frecuencia='Mensual'
 group by nombre_programa 
