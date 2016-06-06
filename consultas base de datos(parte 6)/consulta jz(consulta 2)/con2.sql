18173810e8ea983261c587825f15d5dd7e04f271
Joaquin Zabala <joaco_zabala@hotmail.com.ar>
2016-06-05 23:06:53 -0300

    correccion de consulta jz

--- consultas base de datos(parte 6)/consulta jz(consulta 2)/con2.sql
+++ consultas base de datos(parte 6)/consulta jz(consulta 2)/con2.sql
@@ -1,4 +1,3 @@
-select dni,ocupacion,cuil_cuit
-from donante natural join aporta
-group by dni
-having COUNT(nombre_programa)>2;
+select dni, n_y_ap as "nombre y apellido",ocupacion,cuil_cuit as "cuil/cuit"
+from (select dni,ocupacion,cuil_cuit from donante natural join aporta group by dni having COUNT(nombre_programa)>2) as don_ac
+natural join persona;
